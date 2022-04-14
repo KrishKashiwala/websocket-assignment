f611d17bac13c882b4b6d7692df75c65b48d8a70
krishkashiwala <krishkashiwala@gmail.com>
2022-04-14 13:21:09 +0530

    updated code

--- server/index.js
+++ server/index.js
@@ -5,7 +5,26 @@ const server = require('http').createServer(app);
 const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ server: server });
-let cOne = 0, cTwo = 2, cThree = 3
+let cOne = 0, cTwo = 0, cThree = 0
+setInterval(() => {
+	cOne += 1
+	cTwo += 2
+	cThree += 3
+}, 1000)
+
+
+// max condition
+if (cOne === 10000) {
+	cOne = 0
+}
+if (cTwo === 10000) {
+	cTwo = 0
+}
+if (cThree === 10000) {
+	cThree = 0
+}
+
+
 wss.on('connection', function connection(ws) {
 
 	ws.on('message', (message) => {
@@ -13,15 +32,12 @@ wss.on('connection', function connection(ws) {
 		console.log('received:', inMessage);
 		if (inMessage.value) {
 			if (inMessage.uni == "cOne") {
-				cOne += 1
 				ws.send(JSON.stringify({ msg: cOne, uni: "span1" }))
 			}
 			else if (inMessage.uni == "cTwo") {
-				cTwo += 2
 				ws.send(JSON.stringify({ msg: cTwo, uni: "span2" }))
 			}
 			else if (inMessage.uni == "cThree") {
-				cThree += 3
 				ws.send(JSON.stringify({ msg: cThree, uni: "span3" }))
 			}
 		}
